b3b71cedf347608760af720a26a420f115069fb7
Vikhil <vikhil25@gmail.com>
2025-03-09 12:15:24 +0530

    Added PacManGame class v0.1

---
+++ PacManGame.java
@@ -0,0 +1,15 @@
+package Pac-Man;
+
+import javax.swing.*; //For JFrame, JPanel
+
+public class PacManGame {
+    public static void main(String args[]) {
+        JFrame window = new JFrame("Pac-Man Game");
+        GamePanel gamePanel = new GamePanel(); // For all rendering
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Direct exit on close button activation
+        window.add(gamePanel);
+        window.pack(); // Resizes the window to exactly fit the components
+        window.setVisible(true);
+        gamePanel.start(); // starts the game loop
+    }
+}
