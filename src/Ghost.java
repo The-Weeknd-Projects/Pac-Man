efe503400cdb9a5e810a05395668fedc708c3bdb
codebug53 <azhar1692005@gmail.com>
2025-05-16 19:57:25 +0530

    Added three ghosts

--- src/Ghost.java
+++ src/Ghost.java
@@ -3,16 +3,15 @@ package src;
 import java.awt.*;
 import javax.swing.ImageIcon;
 
-public class Ghost {
-    private int x, y; // start position
-    private int speed;
-    private Image ghostImage; // Image class from java.awt package
-
-    public Ghost(int startX, int startY, int speed, String path) {
-        this.x = startX;
-        this.y = startY;
-        this.speed = speed;
-        this.ghostImage = new ImageIcon(getClass().getResource(path)).getImage();
+public abstract class Ghost{
+    
+    public int speed=10;// Image class from java.awt package
+    public int x, y; // start position
+    public Image ghostImage;
+    public Ghost(int startX, int startY, String path) {
+        x = startX;
+        y = startY;
+        ghostImage = new ImageIcon(getClass().getResource(path)).getImage();
         /*
          * getClass() -> Makes the path relative so that it works on any computer
          * getResource() -> Returns URL Object
@@ -20,19 +19,23 @@ public class Ghost {
          * getImage() -> extracts image from ImageIcon object
          */
     }
-
     public int getX() {
-        return x;
+    	return x;
     }
-
     public int getY() {
-        return y;
+       return y;
     }
-
     public void draw(Graphics2D g2d, int xOffset, int yOffset) {
         g2d.drawImage(ghostImage, xOffset + x, yOffset + y, 30, 30, null);
     }
+    public abstract void move();
+}
 
+class DeadPool extends Ghost{
+    public DeadPool(int startX, int startY, String path) {
+	super(startX, startY, path);
+    }
+    @Override
     public void move() {
         int dir = (int) (Math.random() * 4);
         switch (dir) {
@@ -57,5 +60,80 @@ public class Ghost {
          */
         x = (x + 600) % 600;
         y = (y + 600) % 600;
+    }
+ 
+}
+
+class Gian extends Ghost{
+    public Gian(int startX, int startY, String path) {
+	super(startX, startY, path);
+    }
+    @Override
+    public void move() {
+        int dir = (int) (Math.random() * 4);
+        switch (dir) {
+            case 0:
+                y -= speed;
+                break;
+            case 1:
+                y += speed;
+                break;
+            case 2:
+                x -= speed;
+                break;
+            case 3:
+                x += speed;
+                break;
+     }
+    }
+
+}
+
+class AlienX extends Ghost{
+    public AlienX(int startX, int startY, String path) {
+	super(startX, startY, path);
+    }
+    @Override
+    public void move() {
+        int dir = (int) (Math.random() * 4);
+        switch (dir) {
+            case 0:
+                y -= speed;
+                break;
+            case 1:
+                y += speed;
+                break;
+            case 2:
+                x -= speed;
+                break;
+            case 3:
+                x += speed;
+                break;
+     }
+    }
+
+}
+
+class Gamora extends Ghost{
+    public Gamora(int startX, int startY, String path) {
+	super(startX, startY, path);
+    }
+    @Override
+    public void move() {
+        int dir = (int) (Math.random() * 4);
+        switch (dir) {
+            case 0:
+                y -= speed;
+                break;
+            case 1:
+                y += speed;
+                break;
+            case 2:
+                x -= speed;
+                break;
+            case 3:
+                x += speed;
+                break;
+     }
     }
 }
